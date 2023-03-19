9826873e5c0cc5a11c1d7dbe284f516bc5e8cd96
--add <nastase4ka@mail.ru>
2023-03-19 22:43:52 +0300

    modofied file

--- src/main/java/org/example/HW4.java
+++ src/main/java/org/example/HW4.java
@@ -1,2 +1,4 @@
-package org.example;public class HW4 {
+package org.example;
+
+public class HW4 {
 }
--- src/main/java/org/example/Seminar5.java
+++ src/main/java/org/example/Seminar5.java
@@ -1,2 +1,34 @@
-package org.example;public class Seminar5 {
+package org.example;
+
+import java.util.*;
+
+public class Seminar5 {
+    public static void main(String[] args) {
+        HashMap<Integer, String> hashMap = new HashMap<>();
+        for (int i=0; i <10; i++) {
+            hashMap.put(i, String.valueOf(i));
+            hashMap.putIfAbsent(i, String.valueOf(i) + "!");
+        }
+        //System.out.println(hashMap);
+        //hashMap.forEach((k, v) -> System.out.print(v + " "));
+        Set<Integer> keys = hashMap.keySet(); // возвращает список из неповторяющхся элементов
+        for (Integer k: keys) {
+            System.out.print(hashMap.get(k) + " ");
+        }
+
+//        Iterator<Integer> iterator = keys.iterator();
+//
+//        while(iterator.hasNext()){
+//            String v = hashMap.get(iterator.next());
+//            System.out.println(v + ", ");
+//        }
+        Collection<String> vl = hashMap.values();
+        Set<Map.Entry<Integer, String>> e = hashMap.entrySet();
+        hashMap.compute(1, (k, va) -> v += "!");
+
+        hashMap.getOrDefault(21, "Key is not found");
+        hashMap.computeIfPresent(3, (k, v) -> v += 1);
+        hashMap.computeIfAbsent(78, v -> "1");
+        hashMap.containsKey(31);
+    }
 }
